456b3256c22434bd33d8512457b11ae790374a84
Tom Cameron <tomc603@gmail.com>
2021-03-26 15:55:07 -0400

    Cleanup Allocate functions
    Use os.Truncate instead of creating a file, then truncating it in non-Linux environments.
    Return the value of the truncate error since it's the value we're looking for anyway.
    In Linux environments, attempt to use the existing file, or Truncate it if a smaller size has been requested. This prevents unnecessary churn on multiple runs when using the `-keep` flag.
    Use the returned value for Allocate in Linux environments rather than explicitly returning `nil` on success.

--- allocate.go
+++ allocate.go
@@ -8,23 +8,11 @@ import (
 )
 
 func Allocate(path string, size int64) error {
-	f, openErr := os.Create(path)
-	if openErr != nil {
-		log.Printf("ERROR: Unable to create %s. %s", path, openErr)
-		return openErr
-	}
-
-	if allocErr := f.Truncate(size); allocErr != nil {
-		log.Printf("Allocate(): ERROR: Unable to Truncate(%d) %s. %s", size, path, allocErr)
-		return allocErr
-	}
-
-	if closeErr := f.Close(); closeErr != nil {
+	truncErr := os.Truncate(path, size)
+	if truncErr != nil {
 		if Debug {
-			log.Printf("Allocate(): ERROR: Unable to Close() %s. %s\n", path, closeErr)
+			log.Printf("Allocate(): ERROR: Unable to Truncate(%d) %s. %s", size, path, truncErr)
 		}
-		return closeErr
 	}
-
-	return nil
+	return truncErr
 }
--- allocate_linux.go
+++ allocate_linux.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"os"
 	"syscall"
@@ -11,8 +12,39 @@ const (
 )
 
 func Allocate(path string, size int64) error {
+	s, statErr := os.Stat(path)
+	if statErr != nil {
+		if !errors.Is(statErr, os.ErrNotExist) {
+			log.Printf("Unable to access existing file %s. %s\n", path, statErr)
+			return statErr
+		}
+	}
+
+	// The file exists, so let's see if we can optimize away the fallocate.
+	if s != nil {
+		// If the requested size is the same as the existing file size, let's leave it alone for optimization's sake.
+		if s.Size() == size {
+			if Debug {
+				log.Printf("Allocate(): Requested size and existing file size are the same. Skipping.\n")
+			}
+			return nil
+		}
+
+		// The target size is smaller than the existing file size. Let's truncate it to the requested size.
+		if s.Size() > size {
+			truncErr := os.Truncate(path, size)
+			if truncErr != nil {
+				if Debug {
+					log.Printf("Allocate(): ERROR: Unable to truncate %s. %s\n", path, truncErr)
+				}
+			}
+			return truncErr
+		}
+	}
+
 	// Open the file for read/write, and create it if necessary
 	f, openErr := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0666)
+	defer f.Close()
 	if openErr != nil {
 		if Debug {
 			log.Printf("Allocate(): ERROR: Unable to OpenFile(%s, %d, %d). %s\n", path, os.O_RDWR|os.O_CREATE, 0666, openErr)
@@ -22,19 +54,11 @@ func Allocate(path string, size int64) error {
 
 	// fallocate(2) the file to the requested size. This will zero the entire file's size and allocate
 	// all metadata to guarantee space is available.
-	if allocErr := syscall.Fallocate(int(f.Fd()), FALLOC_FL_ZERO_RANGE, 0, size); allocErr != nil {
+	allocErr := syscall.Fallocate(int(f.Fd()), FALLOC_FL_ZERO_RANGE, 0, size)
+	if allocErr != nil {
 		if Debug {
 			log.Printf("Allocate(): ERROR: Unable to fallocate(%d, 0, %d) %s. %s\n", f.Fd(), size, path, allocErr)
 		}
-		return allocErr
 	}
-
-	if closeErr := f.Close(); closeErr != nil {
-		if Debug {
-			log.Printf("Allocate(): ERROR: Unable to Close() %s. %s\n", path, closeErr)
-		}
-		return closeErr
-	}
-
-	return nil
+	return allocErr
 }
